b0aeb6e096e2bb0ab522e499bef5129135000b6e
Utk Utk <utkarsh.kumar@oberoi-is.org>
2025-06-29 00:28:31 +0530

    hi

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  env: {
+    NEXT_PUBLIC_BASE_URL: 'https://classlogger.com',
+  },
+};
 
 export default nextConfig;
